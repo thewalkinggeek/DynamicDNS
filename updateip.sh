688eecfa19f40ae101a41293b42fb65103127fe0
TheWalkingGeek <deities2001@yahoo.com>
2016-09-26 17:29:06 -0400

    Update updateip.sh

--- updateip.sh
+++ updateip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # You MUST EDIT these settings - Do not remove quotes #
-CURRENTIPLOG="/tmp/whatever.log"                                 # Current IP log location (Logs current IP that is then compared to new IP )
+CURRENTIPLOG="/tmp/whatever.log"                              # Current IP log location (Logs current IP that is then compared to new IP )
 DNSLOG="/tmp/DynamicDnsProvider.log"                          # Dynamic DNS Log File (Logs IP Changes from Dyanmic DNS Provider)
 UPDATEURL="https://myprovider.com/update.php?myupdatekey="    # Update URL provided by Dynamic DNS Service Provider (Get URL from Dynamic DNS Provider)
 
